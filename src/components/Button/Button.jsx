2fe6d3e00d75458def7de409928497990ed891e3
Dawn Carrasco <dawn.carrasco07@gmail.com>
2023-05-31 15:59:37 +0800

    task: initialized project

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './button.css';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ state, variation, text }) => {
+  const [_state, setState] = React.useState(state);
+
+  React.useEffect(() => {
+    setState(state);
+  }, [state]);
+
+  const handleMouseEnter = () => {
+    if (_state !== 'active' && _state !== 'disabled') {
+      setState('hover');
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (_state !== 'active' && _state !== 'disabled') {
+      setState(state);
+    }
+  };
+
+  const handleClick = () => {
+    setState('active');
+    alert('You have clicked me!')
+  };
+
+  return (
+    <button
+      type="button"
+      className={`${variation} ${_state}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      disabled={_state === 'disabled'}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  state: 'default',
+  variation: 'primary',
+  text: 'Button',
+};
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,78 @@
+import { Button } from '../components/Button/Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    text: {
+      description: 'Text or label of the button',
+      control: { type: 'text' },
+    },
+    state: {
+      description: 'State of the button',
+      options: ['default', 'hover', 'active', 'disabled'],
+      control: { type: 'select' },
+    },
+    variation: {
+      description: 'Variation of the button',
+      options: ['primary', 'secondary', 'success', 'warning', 'danger'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+export const Primary = {
+  args: {
+    text: 'Primary',
+  },
+};
+
+export const Secondary = {
+  args: {
+    text: 'Secondary',
+    variation: 'secondary',
+  },
+};
+
+export const Success = {
+  args: {
+    text: 'Success',
+    variation: 'success',
+  },
+};
+
+export const Warning = {
+  args: {
+    text: 'Warning',
+    variation: 'warning',
+  },
+};
+
+export const Danger = {
+  args: {
+    text: 'Danger',
+    variation: 'danger',
+  },
+};
+
+export const Hover = {
+  args: {
+    text: 'Hover',
+    state: 'hover',
+  },
+};
+
+export const Active = {
+  args: {
+    text: 'Active',
+    state: 'active',
+  },
+};
+
+export const Disabled = {
+  args: {
+    text: 'Disabled',
+    state: 'disabld',
+  },
+};
